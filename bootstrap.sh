a68237333458881f89aa22f589648999ea906f11
Chris Christie <chris.christie@hs2solutions.com>
2016-06-09 09:00:39 -0500

    with redis

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,33 @@ apt-get install -y git
 curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
 # Then install with:
 sudo apt-get install -y nodejs
+
+
+
+mkdir /opt/redis
+mkdir /opt/redis/bin
+cd /opt/redis
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+
+make
+
+cp src/redis-server /opt/redis/bin/redis-server
+cp src/redis-cli /opt/redis/bin/redis-cli
+
+cp /vagrant/redis.init.d /etc/init.d/redis
+cp /vagrant/redis.conf /etc/redis.conf
+
+mkdir /var/redis
+chmod -R 777 /var/redis
+
+useradd redis
+
+sudo touch /var/log/redis.log
+sudo chown redis /var/log/redis.log
+sudo chgrp redis /var/log/redis.log
+
+chmod 755 /etc/init.d/redis
+/etc/init.d/redis start
